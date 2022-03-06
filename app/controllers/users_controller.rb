8e426029cfdaeff0e942a1a4e931447ad1a2de9a
EC2 Default User <ec2-user@ip-172-31-12-33.ec2.internal>
2022-03-06 08:04:39 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,43 @@
 class UsersController < ApplicationController
+  
+  
+  
+  def index
+    @book = Book.new
+    @user = current_user
+    @users = User.all
+  end
+  
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+  end
+  
+  before_action :ensure_current_user, {only: [:edit]}
+  def ensure_current_user
+    if current_user.id != User.find(params[:id]).id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+      flash[:notice] = "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
+  get "home/about" => "homes#about",as: "about"
+  
+  resources :books, only:[:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only:[:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
